179ef14d3e23118c3e6d7604fb3a69ec1f94c99d
manhhung241199 <hungvmgch17607@fpt.edu.vn>
2019-12-29 16:01:14 +0700

    hungATN

--- index.php
+++ index.php
@@ -14,16 +14,12 @@
     <div class="content">
         
         <span style="font-size:12px;"><span style="font-family:arial,helvetica,sans-serif;"><form>
-  <h1>Login</h1>
-  <input placeholder="Username" type="text" required="">
-  <input placeholder="Password" type="password" required="">
-  <button>Submit</button>
-</form></span></span>
-        <form action="managing.php" method="POST">
-            <input class="input-information" type="text" name="aduser" placeholder="Username"> <br>
-            <input class="input-information" type="password" name="adpass" placeholder="Password"> <br>
-            <input class="input-information" type="submit" value="Login">
-        </form>
+        <h1>Login</h1>
+        <input placeholder="Username" type="text" required="">
+        <input placeholder="Password" type="password" required="">
+        <button>Submit</button>
+        </form></span></span>
+        
 </body>
 
 </html>
