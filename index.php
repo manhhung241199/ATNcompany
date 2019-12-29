e435de4f8150efc8459354ed43043efd5d35d1af
manhhung241199 <hungvmgch17607@fpt.edu.vn>
2019-12-30 02:09:42 +0700

    hungATN

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <img src="./images/logo1.jpg" width="150" height="300">
+    <img src="./images/logo1.jpg" width="300" height="150">
     <div class="content">
         <h1>Login</h1>
         <form action="managing.php" method="POST">
