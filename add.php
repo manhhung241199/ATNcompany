e4c674d52fc5a20eeeaca93ebee09c70251ca9b3
manhhung241199 <hungvmgch17607@fpt.edu.vn>
2019-12-30 02:18:20 +0700

    hungATN

--- add.php
+++ add.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <img src="./images/logo1.jpg" width="300" height="150">
+    <img src="./images/logo1.jpg" width="320" height="150">
     <div class="content">
         <h1>Add Product</h1>
         <?php 
